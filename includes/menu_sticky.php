fc4821a3c4af1607101f1c4654e89698ff7150a0
nicolaskribas <nicolaskribas@gmail.com>
2018-11-22 02:10:20 -0200

    removido meu caminhao

--- includes/menu_sticky.php
+++ includes/menu_sticky.php
@@ -3,18 +3,17 @@
 	<ul>
 		<a href="index.php"><li>Home</li></a>
 		<a href="fretes.php"><li>Minha lista</li></a>
-		<?php  
+		<?php
 		if ($_SESSION['usuario']['fl_tipo']=="E") {
-		?>	
+		?>
 		<a href="cadastro_frete.php"><li>Cadastro de fretes</li></a>
 		<a href="fretes_historico.php"><li>Fretes concluídos</li></a>
-		<?php  
+		<?php
 		}else{
 		?>
 		<a href="fretes_historico.php"><li>Serviços prestados</li></a>
-		<a href="caminhoes.php"><li>Meus caminhões</li></a>
-		<?php 
-		} 
+		<?php
+		}
 		?>
 		<a href="relatorios.php"><li>Gráfico</li></a>
 		<a href="sair.php"><li>Sair</li></a>
